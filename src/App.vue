76ecc698baa21d22af5aff4deff1a8ee06aa197b
Syed Saif <shahsaif99@gmail.com>
2025-02-21 03:35:37 +0500

    feat: homepage UI

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <v-app-bar app color="primary" elevation="1" dark>
       <v-container class="py-0 fill-height">
         <Logo class="mr-5" />
-        
+
         <v-spacer></v-spacer>
 
         <!-- Desktop Navigation -->
@@ -22,25 +22,25 @@
         </div>
 
         <!-- Mobile Navigation Button -->
-        <v-btn 
-          icon 
+        <v-btn
+          icon
           @click="drawer = !drawer"
           class="d-md-none"
         >
           <v-icon>mdi-menu</v-icon>
         </v-btn>
 
-        <v-btn 
-          color="white" 
-          class="ml-4 d-none d-md-inline-flex" 
+        <v-btn
+          color="white"
+          class="ml-4 d-none d-md-inline-flex"
           to="/contact"
           variant="outlined"
         >
           Contact Us
         </v-btn>
-        <v-btn 
-          color="white" 
-          class="ml-4 d-inline-flex d-md-none" 
+        <v-btn
+          color="white"
+          class="ml-4 d-inline-flex d-md-none"
           to="/contact"
           variant="outlined"
           icon
@@ -51,8 +51,8 @@
     </v-app-bar>
 
     <!-- Mobile Navigation Drawer -->
-    <v-navigation-drawer 
-      v-model="drawer" 
+    <v-navigation-drawer
+      v-model="drawer"
       app
       temporary
       location="top"
@@ -60,9 +60,9 @@
     >
       <v-list>
         <v-list-item-group>
-          <v-list-item 
-            v-for="item in menuItems" 
-            :key="item.title" 
+          <v-list-item
+            v-for="item in menuItems"
+            :key="item.title"
             :to="item.to"
             @click="drawer = false"
           >
@@ -82,7 +82,7 @@
       <v-container>
         <v-row>
           <v-col cols="12" md="4">
-            <h3 class="text-h6 mb-4">Samplers Inc</h3>
+            <h3 class="text-h6 mb-4 font-weight-bold">Samplers Inc</h3>
             <p class="mb-4">Leading the way in product sampling and brand activation solutions.</p>
             <div class="d-flex">
               <v-btn icon="mdi-facebook" variant="text" class="mr-2" href="https://www.facebook.com/SamplersInc/" target="_blank"></v-btn>
@@ -90,70 +90,70 @@
               <v-btn icon="mdi-instagram" variant="text" href="https://www.instagram.com/samplersinc/" target="_blank"></v-btn>
             </div>
           </v-col>
-          
+
           <v-col cols="12" md="8">
             <v-row>
               <v-col cols="12" sm="4">
-                <h4 class="text-h6 mb-4">Solutions</h4>
-                <v-list density="compact" nav class="pa-0">
-                  <v-list-item v-for="item in footerSolutions" 
-                               :key="item.title" 
+                <h4 class="text-h6 mb-4 font-weight-bold">Solutions</h4>
+                <v-list density="compact" nav class="pa-0 bg-transparent">
+                  <v-list-item v-for="item in footerSolutions"
+                               :key="item.title"
                                density="compact"
                                class="pa-0"
                                min-height="24"
                   >
-                    <router-link :to="item.to" class="text-body-2">{{ item.title }}</router-link>
+                    <router-link :to="item.to" class="text-body-2 text-black  text-decoration-none footer-title">{{ item.title }}</router-link>
                   </v-list-item>
                 </v-list>
               </v-col>
-              
+
               <v-col cols="12" sm="4">
-                <h4 class="text-h6 mb-4">Company</h4>
-                <v-list density="compact" nav class="pa-0">
-                  <v-list-item v-for="item in footerCompany" 
-                               :key="item.title" 
+                <h4 class="text-h6 mb-4 font-weight-bold">Company</h4>
+                <v-list density="compact" nav class="pa-0 bg-transparent">
+                  <v-list-item v-for="item in footerCompany"
+                               :key="item.title"
                                density="compact"
                                class="pa-0"
                                min-height="24"
                   >
-                    <router-link :to="item.to" class="text-body-2">{{ item.title }}</router-link>
+                    <router-link :to="item.to" class="text-body-2 text-black  text-decoration-none footer-title ">{{ item.title }}</router-link>
                   </v-list-item>
                 </v-list>
               </v-col>
-              
+
               <v-col cols="12" sm="4">
-                <h4 class="text-h6 mb-4">Contact</h4>
-                <v-list density="compact" nav>
-                  <v-list-item>
+                <h4 class="text-h6 mb-4 font-weight-bold">Contact</h4>
+                <v-list density="compact" nav class="pa-0 bg-transparent">
+                  <v-list-item class="footer-list-item">
                     <template v-slot:prepend>
                       <v-icon color="primary">mdi-map-marker</v-icon>
                     </template>
-                    <v-list-item-title>
+                    <v-list-item-title class="ml-2">
                       Samplers, Inc.<br>
                       530 Smithfield Ave<br>
                       Pawtucket, RI 02860
                     </v-list-item-title>
                   </v-list-item>
-                  <v-list-item>
+                  <v-list-item class="footer-list-item">
                     <template v-slot:prepend>
                       <v-icon color="primary">mdi-phone</v-icon>
                     </template>
-                    <v-list-item-title>401.722.4800</v-list-item-title>
+                    <v-list-item-title class="ml-2">401.722.4800</v-list-item-title>
                   </v-list-item>
-                  <v-list-item>
+                  <v-list-item class="footer-list-item">
                     <template v-slot:prepend>
                       <v-icon color="primary">mdi-email</v-icon>
                     </template>
-                    <v-list-item-title>
+                    <v-list-item-title class="ml-2">
                       For General Info:<br>
                       info@samplersinc.com
                     </v-list-item-title>
                   </v-list-item>
-                  <v-list-item>
+                  <v-list-item class="footer-list-item">
                     <template v-slot:prepend>
                       <v-icon color="primary">mdi-email</v-icon>
                     </template>
-                    <v-list-item-title>
+                    <v-list-item-title class="ml-2">
                       To Contact Management:<br>
                       management@samplersinc.com
                     </v-list-item-title>
@@ -163,9 +163,9 @@
             </v-row>
           </v-col>
         </v-row>
-        
+
         <v-divider class="my-4"></v-divider>
-        
+
         <div class="text-center">
           © {{ new Date().getFullYear() }} Samplers Inc. All rights reserved.
         </div>
@@ -196,3 +196,24 @@ export default {
   })
 }
 </script>
+<style lang="scss">
+.footer-list-item{
+  padding-left: 0px;
+
+  .v-list-item__prepend
+  {
+    .v-list-item__spacer
+    {
+      display: none;
+    }
+  }
+}
+.footer-title{
+  &:hover {
+    text-decoration: underline !important;
+
+  }
+
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,18 +9,19 @@
               :src="withBase('/images/hero-image.jpg')"
               height="600"
               cover
+              class="fade-in"
             />
             <div class="hero-content d-flex flex-column fill-height justify-center align-center text-white text-center">
-              <h1 class="text-h4 font-weight-bold mb-4">
+              <h1 class="text-h2 font-weight-bold mb-4 slide-down">
                 Full-Service Promotional Marketing Solutions
               </h1>
-              <p class="text-h6 mb-8">
+              <p class="text-h5 mb-8 slide-down-delay-1">
                 Delivering turnkey marketing programs from concept to execution
               </p>
               <v-btn
                 x-large
                 color="primary"
-                class="text-none"
+                class="text-none slide-down-delay-2"
                 href="#services"
               >
                 Learn More
@@ -31,40 +32,46 @@
       </v-row>
     </v-container>
 
-    
+
+
 
     <!-- Services Section -->
+    <div class="services-section">
     <v-container id="services" class="py-12">
       <v-row class="text-center mb-12">
-        <v-col cols="12">
-          <h2 class="text-h5 font-weight-bold mb-4">Comprehensive Marketing Services</h2>
+        <v-col cols="12" class="services-fade-up">
+          <h2 class="text-h4 font-weight-bold mb-4">Comprehensive Marketing Services</h2>
           <p class="text-body-1">End-to-end promotional marketing solutions for modern brands</p>
         </v-col>
       </v-row>
 
       <v-row>
         <v-col
-          v-for="service in services"
+          v-for="(service, index) in services"
           :key="service.title"
           cols="12"
           md="4"
+          :class="'services-card-animate-' + index"
         >
           <v-card
-            class="mx-auto"
+            class="mx-auto rounded-lg"
             height="100%"
+            :elevation="2"
           >
             <v-card-item>
-              
-              <v-card-title class="text-h6 mb-3">
+              <v-card-title class="text-h6 mb-3 d-flex flex-column">
                 <v-icon
-                size="48"
-                color="primary"
-                class="mb-4"
-              >
-                {{ service.icon }}
-              </v-icon> {{ service.title }}
+                  size="48"
+                  color="primary"
+                  class="mb-4"
+                >
+                  {{ service.icon }}
+                </v-icon>
+                <div class="font-weight-bold">
+                  {{ service.title }}
+                </div>
               </v-card-title>
-              <v-card-text>
+              <v-card-text class="pl-0">
                 {{ service.description }}
               </v-card-text>
             </v-card-item>
@@ -72,35 +79,36 @@
         </v-col>
       </v-row>
     </v-container>
-
-    <!-- Stats Section -->
-    <v-container fluid class="bg-grey-lighten-4 py-12">
-      <v-container>
-        <v-row>
-          <v-col
-            v-for="stat in stats"
-            :key="stat.value"
-            cols="12"
-            md="3"
-            class="text-center"
-          >
-            <h3 class="text-h3 font-weight-bold primary--text mb-2">
-              {{ stat.value }}
-            </h3>
-            <p class="text-h6">{{ stat.label }}</p>
-          </v-col>
-        </v-row>
-      </v-container>
+  </div>
+    <!-- Stats Section with counter animation -->
+    <v-container fluid class="py-12 stats-section">
+    <v-container>
+      <v-row>
+        <v-col
+          v-for="(stat, index) in stats"
+          :key="stat.value"
+          cols="12"
+          md="3"
+          class="text-center"
+          ref="statCol"
+        >
+          <h3 class="text-h3 font-weight-bold primary--text mb-2">
+            {{ displayedStats[index] }}
+          </h3>
+          <p class="text-h6">{{ stat.label }}</p>
+        </v-col>
+      </v-row>
     </v-container>
+  </v-container>
 
     <!-- CTA Section -->
-    <v-container class="py-12">
-      <v-row align="center" justify="center">
+    <v-container class="py-12 CTA rounded-lg">
+      <v-row align="center py-12" justify="center">
         <v-col cols="12" md="8" class="text-center">
-          <h2 class="text-h3 font-weight-bold mb-4">
+          <h2 class="text-h3 font-weight-bold mb-4 text-white">
             Ready to Grow Your Brand?
           </h2>
-          <p class="text-h6 mb-8">
+          <p class="text-h6 mb-8 text-white">
             Let's discuss how we can help you reach your goals
           </p>
           <v-btn
@@ -152,14 +160,78 @@ export default {
       { value: '50K+', label: 'Events Completed' },
       { value: '1000+', label: 'Brand Partners' },
       { value: '25+', label: 'Years Experience' }
-    ]
+    ],
+    displayedStats: ['1M+', '0+', '0+', '0+'],
+    statsAnimated: false
   }),
   methods: {
-    withBase
+    withBase,
+    animateNumber(index, start, end, duration = 2000) {
+      // Skip animation for 1M+ value
+      if (end === '1M+') return;
+
+      const startNum = parseInt(start.replace(/\D/g, ''));
+      const endNum = parseInt(end.replace(/\D/g, ''));
+      const suffix = end.includes('+') ? '+' : '';
+      let unit = '';
+
+      // Handle K suffix for thousands
+      if (end.includes('K')) {
+        unit = 'K';
+      }
+
+      const range = endNum - startNum;
+      const startTime = Date.now();
+
+      const updateNumber = () => {
+        const currentTime = Date.now();
+        const elapsed = currentTime - startTime;
+
+        if (elapsed > duration) {
+          this.displayedStats[index] = end;
+          return;
+        }
+
+        const progress = elapsed / duration;
+        const currentNum = Math.floor(startNum + (range * progress));
+        this.displayedStats[index] = currentNum + unit + suffix;
+
+        requestAnimationFrame(updateNumber);
+      };
+
+      requestAnimationFrame(updateNumber);
+    },
+    handleIntersection(entries) {
+      entries.forEach(entry => {
+        if (entry.isIntersecting && !this.statsAnimated) {
+          this.statsAnimated = true;
+          this.stats.forEach((stat, index) => {
+            // Skip first stat (1M+) initially to let others catch up a bit
+            if (index === 0) {
+              setTimeout(() => {
+                // Optional: add a quick animation for 1M
+                this.animateNumber(index, '900K+', '1M+', 500);
+              }, 1000); // Start after 1 second
+            } else {
+              this.animateNumber(index, '0+', stat.value);
+            }
+          });
+        }
+      });
+    }
+  },
+  mounted() {
+    const observer = new IntersectionObserver(this.handleIntersection, {
+      threshold: 0.2
+    });
+
+    const statsSection = document.querySelector('.stats-section');
+    if (statsSection) {
+      observer.observe(statsSection);
+    }
   }
 }
 </script>
-
 <style scoped>
 .hero-wrapper {
   position: relative;
@@ -181,5 +253,99 @@ html {
 
 #services {
   scroll-margin-top: 60px; /* Adjust this value to match your fixed top menu's height */
+
 }
-</style> 
+.services-section {
+  background-image: url('/images/services/bgSupport.svg');
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+.CTA{
+  background-image: url('/images/bgFeatures.png');
+  background-size: cover;
+  background-repeat: no-repeat;
+  margin-top: 100px !important;
+  margin-bottom: 100px!important;
+}
+.stats-section {
+  background: hsla(186, 33%, 94%, 1);
+
+background: linear-gradient(90deg, hsla(186, 33%, 94%, 1) 0%, hsla(216, 41%, 79%, 1) 100%);
+
+background: -moz-linear-gradient(90deg, hsla(186, 33%, 94%, 1) 0%, hsla(216, 41%, 79%, 1) 100%);
+
+background: -webkit-linear-gradient(90deg, hsla(186, 33%, 94%, 1) 0%, hsla(216, 41%, 79%, 1) 100%);
+
+filter: progid: DXImageTransform.Microsoft.gradient( startColorstr="#EBF4F5", endColorstr="#B5C6E0", GradientType=1 );
+}
+
+.fade-in {
+  animation: fadeIn 1s ease-out 0.3 ;
+}
+
+.slide-down {
+  animation: slideDown 1s ease-out;
+}
+
+.slide-down-delay-1 {
+  animation: slideDown 1s ease-out 0.9s both;
+}
+
+.slide-down-delay-2 {
+  animation: slideDown 1s ease-out 0.14s both;
+}
+
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+
+@keyframes slideDown {
+  from {
+    opacity: 0;
+    transform: translateY(-20px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+.services-fade-up {
+  opacity: 0;
+  animation: servicesFadeUp 0.8s ease-out forwards;
+}
+
+.services-card-animate-0 {
+  opacity: 0;
+  animation: servicesFadeUp 0.8s ease-out 0.2s forwards;
+}
+
+.services-card-animate-1 {
+  opacity: 0;
+  animation: servicesFadeUp 0.8s ease-out 0.4s forwards;
+}
+
+.services-card-animate-2 {
+  opacity: 0;
+  animation: servicesFadeUp 0.8s ease-out 0.6s forwards;
+}
+
+@keyframes servicesFadeUp {
+  from {
+    opacity: 0;
+    transform: translateY(30px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+
+.services-section {
+  overflow: hidden;
+}
+</style>
